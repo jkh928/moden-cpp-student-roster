c41fb3813cd3329c796879d15a2f1f0e72cf8ebe
Josh Hasam <jhasam2@wgu.edu>
2026-02-25 17:30:25 -0600

    Finalized Student class implementation.
    Completed all member functions in student.cpp. Implemented initializer list for constructor and optimized print() function to match tab-separated rubric requirements. All Student class requirements (D1, D2) are now met.

---
+++ C867_PA/student.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <string>
+#include "student.h"
+
+using namespace std;
+
+// Requirement E2c: Constructor using all input parameters from the table
+Student::Student(const string& studentID, const string& firstName, const string& lastName, const string& emailAddress, int age, int daysToComplete[], DegreeProgram degreeProgram)
+    : studentID(studentID), firstName(firstName), lastName(lastName), emailAddress(emailAddress), age(age), degreeProgram(degreeProgram)
+    {
+    // Using a loop to copy array elements
+        for (int i = 0; i < 3; ++i) {
+            this->daysToComplete[i] = daysToComplete[i];
+        }
+}
+
+// Requirement E2a: Accessors (Getters)
+string Student::GetStudentID() {
+    return this->studentID;
+}
+
+string Student::GetFirstName() {
+    return this->firstName;
+}
+
+string Student::GetLastName() {
+    return this->lastName;
+}
+
+string Student::GetEmailAddress() {
+    return this->emailAddress;
+}
+
+int Student::GetAge() {
+    return this->age;
+}
+
+// Returns a pointer to the array (Competency 4048.2.5)
+int* Student::GetDaysToComplete() {
+    return this->daysToComplete;
+}
+
+DegreeProgram Student::GetDegreeProgram() {
+    return this->degreeProgram;
+}
+
+
+// Requirement E2b: Mutators (Setters)
+void Student::SetStudentID(const string& studentID) {
+    this->studentID = studentID;
+}
+
+void Student::SetFirstName(const string& firstName) {
+    this->firstName = firstName;
+}
+
+void Student::SetLastName(const string& lastName) {
+    this->lastName = lastName;
+}
+
+void Student::SetEmailAddress(const string& emailAddress) {
+    this->emailAddress = emailAddress;
+}
+
+void Student::SetAge(int age) {
+    this->age = age;
+}
+
+void Student::SetDaysToComplete(int daysToComplete[]) {
+    for (int i = 0; i < 3; ++i) {
+        this->daysToComplete[i] = daysToComplete[i];
+    }
+}
+
+void Student::SetDegreeProgram(DegreeProgram degreeProgram) {
+    this->degreeProgram = degreeProgram;
+}
+
+// Requirement E2d: Print specific student data in the required format
+void Student::print() {
+    // 1. Format the output with tabs (\t)
+    cout << this->studentID << "\t";
+    cout << "First Name: " << this->firstName << "\t";
+    cout << "Last Name: " << this->lastName << "\t";
+    cout << "Age: " << this->age << "\t";
+    // 2. Format the days array output
+    cout << "daysInCourse: {" << this->daysToComplete[0] << ", " << this->daysToComplete[1] << ", " << this->daysToComplete[2] << "}\t";
+    cout << "Degree Program: ";
+    // 3. Convert the enum to a string for printing
+    if (this->degreeProgram == SECURITY) cout << "Security";
+    else if (this->degreeProgram == NETWORK) cout << "Network";
+    else if (this->degreeProgram == SOFTWARE) cout << "Software";
+    cout << endl;
+}
